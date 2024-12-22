c2a6de5a848bdad2a5f1628b4f6c0686fb0faeb3
bagheri-dev <bagheri-develop@gmail.com>
2024-12-22 17:30:53 +0330

    Add 'Recommendations' section to display client testimonials

---
+++ containers/recommendations.tsx
@@ -0,0 +1,10 @@
+const Recommendations = () => {
+    return ( 
+        <section className="mt-16">
+            <h3 className="text-center text-3xl font-bold mb-6 dark:text-white">Recommendations</h3>
+            <p className="font-semibold text-center text-sm text-gray-500 dark:text-white">Read what my clients have to say about working with me. I take pride in delivering high-quality front-end development services and providing solutions that align with my clients' goals. Their feedback reflects my commitment to excellence, collaboration, and creating seamless digital experiences. Explore these testimonials to see how I can help bring your next project to life.</p>
+        </section>
+     );
+}
+ 
+export default Recommendations;
